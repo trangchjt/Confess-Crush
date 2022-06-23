63ae2198d77c26a855fdcb2c869fc54f470aa795
trangchjt <108052050+trangchjt@users.noreply.github.com>
2022-06-23 15:15:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const CONFIG = {
     btnYes: 'Anh cũng rất nhớ em 🥺',
     btnNo: 'Anh nhớ em :3',
     question: 'Trên thế giới hơn 7 tỉ nụ cười nhưng đối với em nụ cười của anh vẫn là đẹp nhất <3',
-    btnReply: 'Gửi cho anh iu <3',
-    reply: 'Anh nhớ ăn uống đầy đủ đừng để đau dạ dày <33333333',
-    mess: 'Anh biết mà 🥰. Chắc chắn anh sẽ làm thế 😘😘',
-    messDesc: 'Đáng iu ghê em rất thích anh.',
+    btnReply: 'Gửi cho bé iu <3',
+    reply: 'Còn đối với anh em khóc cũng đẹp <33333333',
+    mess: 'Hôm nay đi làm chắc anh iu cũng rất mệt 🥰. Nhưng đừng quên ăn để đau dạ dày nhaaa 😘😘',
+    messDesc: 'Anh biết mà em iuu',
     btnAccept: 'Anh vô cùng vô cùng thích em <3',
     messLink: 'https://www.rd.com/wp-content/uploads/2020/12/LoveMeme6.jpg' 
 }
